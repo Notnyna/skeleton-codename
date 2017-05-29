25bba0bc33e3943f493243f81dfaa48fcc857c57
SagaBegins <jvsagar95@gmail.com>
2017-05-29 12:37:39 +0300

    OpeningScreen
    Added team gif at start

---
+++ Assets/PlayMovie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class PlayMovie : MonoBehaviour {
+    public float w;
+    private IEnumerator wait;
+    bool canswitch;
+    public KeyCode skip;
+    // Use this for initialization
+    void Start () {
+        canswitch = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        wait = waitfor(w);
+        StartCoroutine(wait);
+        if (Input.GetKeyDown(skip))
+            canswitch = true;
+        if(canswitch)
+        SceneManager.LoadScene("Global");
+	}
+
+    IEnumerator waitfor(float len)
+    {
+        yield return new WaitForSeconds(len);
+        canswitch = true;
+    }
+}
