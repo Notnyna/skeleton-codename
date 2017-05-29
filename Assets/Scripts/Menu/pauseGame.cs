8de2f41180abbbd5c08ecf065b7311a32f398a91
SagaBegins <jvsagar95@gmail.com>
2017-05-29 11:53:12 +0300

    Added PauseScreen

---
+++ Assets/Scripts/Menu/pauseGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseGame : MonoBehaviour {
+    public GameObject pause;
+    public KeyCode pausekey;
+    private bool ispaused;
+	// Use this for initialization
+	void Start ()
+    {
+        ispaused = false;
+        pause.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pause.GetComponent<Transform>().position = GetComponent<Transform>().position;
+        if (Input.GetKeyDown(pausekey))
+        {    
+            ispaused = !ispaused;
+            pause.SetActive(ispaused);
+        }
+        if (ispaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+	}
+}
